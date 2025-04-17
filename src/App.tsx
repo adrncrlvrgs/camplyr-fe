669deb72b83695f47b45819c8ab9bf250d3a9646
adrncrlvrgs <imadrianvargas@gmail.com>
2025-04-17 23:51:14 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <div className="text-black">
+      hello word
+    </div>
+  )
+}
+
+export default App
