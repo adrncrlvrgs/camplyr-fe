ad995dfce3224a284bbb7f6534aea0cdfc15b32e
adrncrlvrgs <imadrianvargas@gmail.com>
2025-04-19 00:51:44 +0800

    add inital navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import { Button } from "./components/Button/Button";
 import LandingPage from "./views/Landing/LandingPage";
 import { Page } from "./components/Pages";
 import "./App.css";
+import PrivateNav from "./components/Navigation/PrivateNav";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <Page>
-      <LandingPage />
-      <div className="font-medium">test</div>
-      <Button>Test</Button>
+      <PrivateNav/>
     </Page>
   );
 }
