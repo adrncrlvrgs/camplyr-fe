dd52f2659c79aa03207824eca5cbb86d426bd529
adrncrlvrgs <imadrianvargas@gmail.com>
2025-04-23 22:14:55 +0800

    enhance inital CardPost

--- src/components/Card/CardPost.tsx
+++ src/components/Card/CardPost.tsx
@@ -1,33 +1,36 @@
 import { Avatar, AvatarImage } from "@/components/Avatar/Avatar";
-import { Button } from "@/components/Button/Button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/Card/Card";
-import { Globe, Bookmark, Send, MessageSquare } from "lucide-react";
+
+import { Card, CardContent, CardHeader } from "@/components/Card/Card";
+import { Globe, Bookmark, Heart } from "lucide-react";
 import { Badge } from "@/components/Badge/Badge";
 
 const CardPost = () => {
   return (
-    <Card className="w-full shadow-sm border border-neutral-200 rounded-2xl p-3">
+    <Card className="sm:max-w-xl shadow-sm border border-neutral-200 rounded-2xl p-3">
       <CardHeader className="flex flex-row items-start p-4">
         <Avatar className="size-14">
           <AvatarImage src="https://github.com/shadcn.png" />
         </Avatar>
-        <div className="pl-4">
-          <div className="font-semibold text-black text-lg">
-            Adrian Vargas
-            <span className="pl-2">
-              <Badge variant="outline">Recruiter</Badge>
-            </span>
+        <div className="flex flex-row justify-between w-full">
+          <div className="pl-4">
+            <div className="font-semibold text-black text-lg">
+              Adrian Vargas
+              <span className="pl-2">
+                <Badge variant="outline">Recruiter</Badge>
+              </span>
+            </div>
+            <p className="text-xs text-muted-foreground">
+              Jr. Software Engineer
+            </p>
+            <div className="flex items-center gap-1 text-xs text-muted-foreground mt-1">
+              <span>Posted 1hr ago</span>
+              <span>·</span>
+              <Globe className="w-3.5 h-3.5" />
+            </div>
           </div>
-          <p className="text-xs text-muted-foreground">Jr. Software Engineer</p>
-          <div className="flex items-center gap-1 text-xs text-muted-foreground mt-1">
-            <span>Posted 1hr ago</span>
-            <span>·</span>
-            <Globe className="w-3.5 h-3.5" />
+          <div className="flex flex-row gap-3">
+            <Heart className="size-5" />
+            <Bookmark className="size-5" />
           </div>
         </div>
       </CardHeader>
@@ -43,7 +46,7 @@ const CardPost = () => {
         </p>
       </CardContent>
 
-      <CardFooter className="pb-2 justify-around px-10 pt-1">
+      {/* <CardFooter className="pb-2 justify-around px-10 pt-1">
         <div className="flex w-full gap-x-20">
           <Button
             variant="ghost"
@@ -69,7 +72,7 @@ const CardPost = () => {
             Message
           </Button>
         </div>
-      </CardFooter>
+      </CardFooter> */}
     </Card>
   );
 };
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,24 +1,37 @@
-import { Avatar, AvatarImage } from "@/components/Avatar/Avatar";
-import { Button } from "@/components/Button/Button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/Card/Card";
 import { Page } from "@/components/Pages";
-import { Globe, Bookmark, Send, MessageSquare } from "lucide-react";
-import { Badge } from "@/components/Badge/Badge";
 import CardPost from "@/components/Card/CardPost";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/Modal/Dialog";
 
 const Index = () => {
   return (
     <Page>
       <div className="flex w-full min-h-[100dvh] justify-center sm:max-w-[85rem] border-x border-dashed border-neutral-400">
-        <div className="flex flex-1 flex-col items-center w-full p-4 gap-4 max-w-3xl">
-          <CardPost/>
-          <CardPost/>
-          <CardPost/>
+        <div className="flex flex-1 flex-col items-center p-4 gap-4 ">
+          {[1, 2, 3].map((_, key) => (
+            <Dialog key={key}>
+              <DialogTrigger asChild>
+                <div className="cursor-pointer w-auto">
+                  <CardPost />
+                </div>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-xl">
+                <DialogHeader>
+                  <DialogTitle>Job Details</DialogTitle>
+                  <DialogDescription>
+                    Here's the full description and actions for the post.
+                  </DialogDescription>
+                </DialogHeader>
+                <p>test</p>
+              </DialogContent>
+            </Dialog>
+          ))}
         </div>
       </div>
     </Page>
