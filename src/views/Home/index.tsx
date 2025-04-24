1aca679c86779d0e2cdf736767ecf5bacf9489d1
adrncrlvrgs <imadrianvargas@gmail.com>
2025-04-24 22:13:10 +0800

    add initial grid layout on Home page

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -12,26 +12,36 @@ import {
 const Index = () => {
   return (
     <Page>
-      <div className="flex w-full min-h-[100dvh] justify-center sm:max-w-[85rem] border-x border-dashed border-neutral-400">
-        <div className="flex flex-1 flex-col items-center p-4 gap-4 ">
-          {[1, 2, 3].map((_, key) => (
-            <Dialog key={key}>
-              <DialogTrigger asChild>
-                <div className="cursor-pointer w-auto">
-                  <CardPost />
-                </div>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-xl">
-                <DialogHeader>
-                  <DialogTitle>Job Details</DialogTitle>
-                  <DialogDescription>
-                    Here's the full description and actions for the post.
-                  </DialogDescription>
-                </DialogHeader>
-                <p>test</p>
-              </DialogContent>
-            </Dialog>
-          ))}
+      <div className="flex flex-col w-full min-h-[100dvh] justify-center sm:max-w-[85rem] border-x border-dashed border-neutral-400">
+        <div className=" grid grid-cols-4 w-full gap-3">
+          <div>
+            <CardPost />
+          </div>
+          <div className="col-span-2 col-start-2 w-full">
+            <div className="flex flex-1 flex-col items-center p-4 gap-4 ">
+              {[1, 2, 3].map((_, key) => (
+                <Dialog key={key}>
+                  <DialogTrigger asChild>
+                    <div className="cursor-pointer w-auto">
+                      <CardPost />
+                    </div>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-xl">
+                    <DialogHeader>
+                      <DialogTitle>Job Details</DialogTitle>
+                      <DialogDescription>
+                        Here's the full description and actions for the post.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <p>test</p>
+                  </DialogContent>
+                </Dialog>
+              ))}
+            </div>
+          </div>
+          <div>
+            <CardPost />
+          </div>
         </div>
       </div>
     </Page>
