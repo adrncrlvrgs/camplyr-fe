c24bec80e9f3dfa76dd0cb48df1c68161cbbb083
adrncrlvrgs <imadrianvargas@gmail.com>
2025-12-06 23:43:37 +0800

    test new

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Home from "@/views/Home";
-// import LandingPage from "@/views/Landing";
-// import Login from "@/views/Login";
+import LandingPage from "@/views/Landing";
+import Login from "@/views/Login";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./App.css";
