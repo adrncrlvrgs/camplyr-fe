b6eec8d516d62f3b1415103cb78fc203403a5f66
Adrian Vargas <imadrianvargas@gmail.com>
2026-08-03 23:06:11 +0800

    create initial module for jobs

--- src/components/shared/Navigation/PrivateNav.tsx
+++ src/components/shared/Navigation/PrivateNav.tsx
@@ -1,15 +1,14 @@
-import React from "react";
 import { Home, Users, Briefcase, MessageSquare, Bell } from "lucide-react";
 import PageHeader from "../Layout/PageHeader";
 
 const PrivateNav = () => {
   const navItems = [
-    { icon: <Home className="w-5 h-5 mx-auto mb-1" />, label: "Home" },
+    { icon: <Home className="w-5 h-5 mx-auto mb-1" />, label: "Home",nav: "/home" },
     {
       icon: <Users className="w-5 h-5 mx-auto mb-1" />,
       label: "Connections",
     },
-    { icon: <Briefcase className="w-5 h-5 mx-auto mb-1" />, label: "Jobs" },
+    { icon: <Briefcase className="w-5 h-5 mx-auto mb-1" />, label: "Jobs", nav: "/jobs"  },
     {
       icon: <MessageSquare className="w-5 h-5 mx-auto mb-1" />,
       label: "Messaging",
@@ -19,11 +18,11 @@ const PrivateNav = () => {
   return (
     <PageHeader>
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-end sm:ps-5 text-black">
-        {navItems.map(({ icon, label }, i) => (
+        {navItems.map(({ icon, label, nav }, i) => (
           <a
             key={i}
             className="flex flex-col items-center justify-center px-4 py-2 text-sm font-medium leading-none hover:text-gray-400 focus:outline-hidden focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
-            href="#"
+            href={nav}
           >
             {icon}
             {label}
