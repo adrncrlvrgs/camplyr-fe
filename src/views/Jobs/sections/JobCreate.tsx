ccc030019a89c496c2e0e7e6c8723e2e96a87ea4
Adrian Vargas <imadrianvargas@gmail.com>
2026-08-05 20:47:12 +0800

    add job create button

---
+++ src/views/Jobs/sections/JobCreate.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/Button";
+
+export default function JobCreate() {
+  return (
+    <div className="space-y-6 mb-2">
+      <Button>Create a Job</Button>
+    </div>
+  );
+}
