d6a47f8da6f6b24fb7ef650faec67586dfdc5a3a
Adrian Vargas <imadrianvargas@gmail.com>
2026-08-06 21:37:22 +0800

    creaated initial job module

--- src/views/Jobs/sections/JobCreate.tsx
+++ src/views/Jobs/sections/JobCreate.tsx
@@ -1,9 +1,33 @@
 import { Button } from "@/components/ui/Button";
+import { PlusIcon } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/Dialog";
 
 export default function JobCreate() {
   return (
-    <div className="space-y-6 mb-2">
-      <Button>Create a Job</Button>
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="space-y-6 mb-2">
+          <Button>
+            Create a Job <PlusIcon size={18} />
+          </Button>
+        </div>
+      </DialogTrigger>
+
+      <DialogContent className="sm:max-w-xl">
+        <DialogTitle>Create a Job</DialogTitle>
+
+        <DialogDescription>
+          Share your thoughts, ideas, or updates.
+        </DialogDescription>
+
+        <div className="mt-4">{/* <CreateJobForm /> */}</div>
+      </DialogContent>
+    </Dialog>
   );
 }
